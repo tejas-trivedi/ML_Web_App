bd8b9ba8d4fa2c011b5601509b3d0c415e695b77
tejas-trivedi <500077185@stu.upes.ac.in>
2020-07-08 20:44:37 +0530

    sidebar

--- app.py
+++ app.py
@@ -52,9 +52,15 @@ def main():
     df = load_data()
    # x_train, x_test, y_train, y_test = split(df)
     class_name = ['edible', 'poisonous']
+    st.sidebar.subheader("Choose Classifier")
+    classifier = st.sidebar.selectbox("Classifier", ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest" ))
     
     
-    
+    if classifier == 'Support Vector Machine (SVM)':
+        st.sidebar.subheader("Model Hyperparameters")
+        C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C')
+        kernel = st.sidebar.radio("Kernel", ("rbf", "Linear"), key='kernel')
+        gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
     
     
     
