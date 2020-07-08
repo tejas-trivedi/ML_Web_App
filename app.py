adbc2faa3fa40cbe1fb4111a071de842a3276737
tejas-trivedi <500077185@stu.upes.ac.in>
2020-07-08 21:45:08 +0530

    logistic regression

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ def main():
     st.sidebar.title("Binary Classification Web App")
     st.sidebar.markdown("Are your mushrooms edible or poisonous?")
     
-    @st.cache(persist=True)
+    #@st.cache(persist=True)
     def load_data():
+        #data = pd.read_csv('C:/Users/TEJAS/Desktop/project/mushrooms.csv')
         data = pd.read_csv('C:/Users/TEJAS/Desktop/project/mushrooms.csv')
         label = LabelEncoder()
         for col in data.columns:
@@ -50,8 +51,8 @@ def main():
             st.pyplot() 
             
     df = load_data()
-   # x_train, x_test, y_train, y_test = split(df)
-    class_name = ['edible', 'poisonous']
+    x_train, x_test, y_train, y_test = split(df)
+    class_names = ['edible', 'poisonous']
     st.sidebar.subheader("Choose Classifier")
     classifier = st.sidebar.selectbox("Classifier", ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest" ))
     
@@ -59,10 +60,43 @@ def main():
     if classifier == 'Support Vector Machine (SVM)':
         st.sidebar.subheader("Model Hyperparameters")
         C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C')
-        kernel = st.sidebar.radio("Kernel", ("rbf", "Linear"), key='kernel')
+        kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
         gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
     
-    
+        metrics = st.sidebar.multiselect("What metric to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+        
+        if st.sidebar.button("Classify", key='classify'):
+            st.subheader("Support Vector Machine (SVM) Results")
+            model = SVC(C=C, kernel=kernel, gamma=gamma)
+            model.fit(x_train, y_train)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy:", accuracy.round(2))
+            st.write("Precision:", precision_score(y_test, y_pred, labels=class_names).round(2))
+            st.write("Recall:", recall_score(y_test, y_pred, labels=class_names).round(2))
+            plot_metrics(metrics)
+
+
+    if classifier == 'Logistic Regression':
+        st.sidebar.subheader("Model Hyperparameters")
+        C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_LR')
+        #kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
+        #gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
+        max_iter = st.sidebar.slider("Maximum number of iterations", 100, 500, key='max_iter')
+        
+        metrics = st.sidebar.multiselect("What metric to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+        
+        if st.sidebar.button("Classify", key='classify'):
+            st.subheader("Logistic Regression Results")
+            model = LogisticRegression(C=C, max_iter=max_iter)
+            model.fit(x_train, y_train)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy:", accuracy.round(2))
+            st.write("Precision:", precision_score(y_test, y_pred, labels=class_names).round(2))
+            st.write("Recall:", recall_score(y_test, y_pred, labels=class_names).round(2))
+            plot_metrics(metrics)
+            
     
     
     
